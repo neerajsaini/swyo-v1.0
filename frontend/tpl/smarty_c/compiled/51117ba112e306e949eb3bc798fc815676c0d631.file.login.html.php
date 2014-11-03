a61b065050acb0587b8e684db01c15d378a63b0c
Neeraj Saini <neeraj.saini.13@gmail.com>
2014-11-03 14:31:01 +0530

    commit

--- frontend/tpl/smarty_c/compiled/51117ba112e306e949eb3bc798fc815676c0d631.file.login.html.php
+++ frontend/tpl/smarty_c/compiled/51117ba112e306e949eb3bc798fc815676c0d631.file.login.html.php
@@ -1,6 +1,6 @@
-<?php /* Smarty version Smarty-3.1.19, created on 2014-11-01 16:18:59
+<?php /* Smarty version Smarty-3.1.19, created on 2014-11-02 20:11:14
          compiled from "/Applications/AMPPS/www/stream/v1.0/frontend/tpl/smarty_t/login/login.html" */ ?>
-<?php /*%%SmartyHeaderCode:17239531685454f9e37516b5-95374601%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:1308948371545681d27e26d7-91895351%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
@@ -17,15 +17,15 @@ $_valid = $_smarty_tpl->decodeProperties(array (
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '17239531685454f9e37516b5-95374601',
+  'nocache_hash' => '1308948371545681d27e26d7-91895351',
   'function' => 
   array (
   ),
   'has_nocache_code' => false,
   'version' => 'Smarty-3.1.19',
-  'unifunc' => 'content_5454f9e37c8254_16388108',
+  'unifunc' => 'content_545681d285fba7_17209689',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_5454f9e37c8254_16388108')) {function content_5454f9e37c8254_16388108($_smarty_tpl) {?><!DOCTYPE html>
+<?php if ($_valid && !is_callable('content_545681d285fba7_17209689')) {function content_545681d285fba7_17209689($_smarty_tpl) {?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
